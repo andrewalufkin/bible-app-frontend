f5b770094ca585f44cf979ca1096a44abebc523c
Andrew Adams <andrewalufkin@gmail.com>
2025-06-07 15:13:50 -0500

    test: check login heading

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login page heading', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = await screen.findByText(/sign in to your account/i);
+  expect(headingElement).toBeInTheDocument();
 });
